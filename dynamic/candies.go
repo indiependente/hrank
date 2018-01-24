599a29353f6adeb8932f6661d16a91ec36480169
Francesco Farina <rockerg991@gmail.com>
2018-01-24 23:39:56 +0000

    candies done!

--- dynamic/candies.go
+++ dynamic/candies.go
@@ -2,74 +2,37 @@ package main
 
 import (
 	"fmt"
+	"hrank/utils"
 )
 
 func main() {
 	var n int
 	fmt.Scanf("%d", &n)
-	ratings := createSliceOfSize(n)
+	ratings := utils.CreateSliceOfSize(n)
 	fmt.Println(getMinCandies(ratings))
 }
 
-func getMinCandies(ratings []int) ([]int, int) {
+func getMinCandies(ratings []int) int {
 	lr := len(ratings)
-	scores := make([]int, lr)
-	var decr bool
-	var dl, el int
+	candies := make([]int, lr)
+
+	// everyone gets a candy
 	for i := 0; i < lr; i++ {
-		if i != lr-1 && ratings[i] > ratings[i+1] {
-			if !decr {
-				decr = true
-				dl = decrLen(ratings, i)
-			}
-			for j := 0; j < dl; j++ {
-				scores[j] = dl - j + 1
-			}
-		} else if ratings[i] < ratings[i+1] && i != 0 {
-			decr = false
-			scores[i] = scores[i-1] + 1
-		} else if ratings[i] == ratings[i+1] {
-			decr = false
-			el = equalLen(ratings, i)
+		candies[i] = 1
+	}
 
+	// first pass - increasing sequence
+	for i := 1; i < lr; i++ {
+		if ratings[i] > ratings[i-1] {
+			candies[i] = candies[i-1] + 1
 		}
 	}
 
-	return scores, sum(scores)
-}
-
-func createSliceOfSize(n int) []int {
-	a := make([]int, n)
-	for i := 0; i < n; i++ {
-		fmt.Scan(&a[i])
-	}
-	return a
-}
-
-func sum(arr []int) int {
-	sum := 0
-	for i := 0; i < len(arr); i++ {
-		sum += arr[i]
-	}
-	return sum
-}
-
-func decrLen(arr []int, pos int) int {
-	length := 0
-	for i := pos; i < len(arr)-1; i++ {
-		if i != len(arr) && arr[i] < arr[i+1] {
-			length++
+	// second pass - decreasing sequence
+	for i := lr - 2; i >= 0; i-- {
+		if ratings[i] > ratings[i+1] && candies[i] < candies[i+1]+1 {
+			candies[i] = candies[i+1] + 1
 		}
 	}
-	return length
-}
-
-func equalLen(arr []int, pos int) int {
-	length := 0
-	for i := pos; i < len(arr)-1; i++ {
-		if i != len(arr) && arr[i] == arr[i+1] {
-			length++
-		}
-	}
-	return length
+	return utils.Sum(candies)
 }
