b3aeb5a2789207f27667c64393e943f99ad0c616
indiependente <rockerg991@gmail.com>
2018-01-24 18:17:57 +0000

    candies: still wip

--- dynamic/candies.go
+++ dynamic/candies.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"hrank/utils"
 )
 
 func main() {
@@ -12,20 +11,31 @@ func main() {
 	fmt.Println(getMinCandies(ratings))
 }
 
-func getMinCandies(ratings []int) int {
-	n := len(ratings)
-	min, max := utils.MinMax(ratings)
-	sToC := make(map[int][]int) // score to candies hashmap
-	prevR := -1                 // previous rating
-	prevS := -1                 // previous score
-	for _, e := range ratings {
-		candies := 0
-		if len(sToC[e]) == 0 {
+func getMinCandies(ratings []int) ([]int, int) {
+	lr := len(ratings)
+	scores := make([]int, lr)
+	var decr bool
+	var dl, el int
+	for i := 0; i < lr; i++ {
+		if i != lr-1 && ratings[i] > ratings[i+1] {
+			if !decr {
+				decr = true
+				dl = decrLen(ratings, i)
+			}
+			for j := 0; j < dl; j++ {
+				scores[j] = dl - j + 1
+			}
+		} else if ratings[i] < ratings[i+1] && i != 0 {
+			decr = false
+			scores[i] = scores[i-1] + 1
+		} else if ratings[i] == ratings[i+1] {
+			decr = false
+			el = equalLen(ratings, i)
 
 		}
-		sToC[e] = append(sToC[e])
 	}
-	return sumArrayOfArray(getValuesFromMap(sToC))
+
+	return scores, sum(scores)
 }
 
 func createSliceOfSize(n int) []int {
@@ -36,18 +46,30 @@ func createSliceOfSize(n int) []int {
 	return a
 }
 
-func getValuesFromMap(m map[int][]int) [][]int {
-	var values [][]int
-	for _, v := range m {
-		values = append(values, v)
+func sum(arr []int) int {
+	sum := 0
+	for i := 0; i < len(arr); i++ {
+		sum += arr[i]
 	}
-	return values
+	return sum
 }
 
-func sumArrayOfArray(array [][]int) int {
-	aSum := 0
-	for _, a := range array {
-		aSum += utils.Sum(a)
+func decrLen(arr []int, pos int) int {
+	length := 0
+	for i := pos; i < len(arr)-1; i++ {
+		if i != len(arr) && arr[i] < arr[i+1] {
+			length++
+		}
 	}
-	return aSum
+	return length
+}
+
+func equalLen(arr []int, pos int) int {
+	length := 0
+	for i := pos; i < len(arr)-1; i++ {
+		if i != len(arr) && arr[i] == arr[i+1] {
+			length++
+		}
+	}
+	return length
 }
